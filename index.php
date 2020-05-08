61d7269d9a48b738c94c50264514268f97156fa2
espinhara <52253458+espinhara@users.noreply.github.com>
2020-05-07 21:04:50 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Teste navigate</title>
+  </head>
+  <body>
+  <form action="cadastro.php" method="post">
+    <input type="text" name="user" placeholder="Username" />
+    <input type="email" name="email" placeholder="E-mail"/>
+    <input type="password" name="senha" placeholder="password" />
+    <input type="submit" value="Cadastrar" />
+  </form>
+  </body>
+</html>
